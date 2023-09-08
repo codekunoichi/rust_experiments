b33c905f88e507ba62957732fa17e8614fd70217
Rumpa Giri <codekunoichi@users.noreply.github.com>
2023-09-08 16:39:03 +0000

    Add Makefile to the application

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,11 @@
+fn add_two_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 fn main() {
+    let a = 3;
+    let b = 4;
+    let c = add_two_numbers(a, b);
+    println!("{} + {} = {}", a, b, c);
     println!("Hello, world!");
 }

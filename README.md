5b75cbc0bb85cc96ff62068611995bdb51e3e2b5
Rumpa Giri <codekunoichi@users.noreply.github.com>
2023-09-08 16:51:33 +0000

    Use AWS Toolkit/Codewhisperer AI Assistance

--- README.md
+++ README.md
@@ -19,3 +19,9 @@
 
 - `Cargo.toml` - Is like the requirements.txt - which describes your dependencies etc.
 - To run the main.rs - you CD into the directory and just do `cargo run`
+
+## Using generative AI tools for code completion
+
+- GitHub Copilot is not free, so an alternative is AWSToolkit
+- AWS Codewhisperer is a free AI Code Pairing Assistance. Use AWS Builder ID to connect VSCode to AWS Codewhisperer.
+- To your email address to sign up for AWS Builder ID for free.
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,10 +2,23 @@ fn add_two_numbers(a: i32, b: i32) -> i32 {
     a + b
 }
 
+// add three numbers together
+fn add_three_numbers(a: i32, b: i32, c: i32) -> i32 {
+    a + b + c
+}
+
 fn main() {
     let a = 3;
     let b = 4;
     let c = add_two_numbers(a, b);
     println!("{} + {} = {}", a, b, c);
     println!("Hello, world!");
+
+    // add three numbers together
+    let d = 5;
+    let e = 6;
+    let f = 7;
+    let g = add_three_numbers(d, e, f);
+    println!("{} + {} + {} = {}", d, e, f, g);
 }
+
